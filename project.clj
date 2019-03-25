5fa1ad03cf7f9d1ee703638e563abe7b8ad54cbe
Chiao Cheng <chiao@alum.mit.edu>
2019-03-25 11:28:49 +0800

    added tr-style-fn

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject re-frame-datatable "0.6.0"
+(defproject chiaolun/re-frame-datatable "0.6.0"
   :description "DataTable component for re-frame 0.8.0+"
-  :url "https://github.com/kishanov/re-frame-datatable"
+  :url "https://github.com/chiaolun/re-frame-datatable"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
--- src/re_frame_datatable/core.cljs
+++ src/re_frame_datatable/core.cljs
@@ -338,6 +338,7 @@
                {:keys [::selection]} state
                {:keys [::table-classes
                        ::tr-class-fn
+                       ::tr-style-fn
                        ::header-enabled?
                        ::extra-header-row-component
                        ::footer-component
@@ -405,7 +406,9 @@
                     (merge
                       {}
                       (when tr-class-fn
-                        (css-class-str (tr-class-fn data-entry))))
+                        (css-class-str (tr-class-fn data-entry)))
+                      (when tr-style-fn
+                        (tr-style-fn data-entry)))
 
                     (when (::enabled? selection)
                       [:td
